a7701b53454109eb5033f955bae89c02cf4be795
Mike Weaver <miketweaver@users.noreply.github.com>
2016-01-15 18:50:22 -0700

    add communications and secretary

--- officers.php
+++ officers.php
@@ -61,10 +61,49 @@ include 'header.php';
           </div>
         </div>
 
+         <!-- Communications -->
+        <div class="item">
+          <img src="img/officers/<?php print($officers_communications_image); ?>" alt="<?php print($officers_communications_name); ?>" />
+          <div class="carousel-caption">
+            <h3>Communications</h3>
+              <h1><a target="_blank" href="<?php print($officers_communications_link); ?>"><?php print($officers_communications_name); ?></a></h1>
+            <p>
+              <strong>Class: </strong> <?php print($officers_communications_class); ?><br>
+              <strong>Hometown: </strong> <?php print($officers_communications_hometown); ?>
+            </p>
+            <ul>
+                <li>
+                  <strong><?php print($officers_communications_question); ?></strong><br>
+                  <?php print($officers_communications_answer); ?>
+                </li>
+            </ul>
+            <p><ul class="contact-icons" style="text-align: center;">
+                <li><a class="<?php print($officers_communications_contact); ?>" target="_blank" href="<?php print($officers_communications_contact_link); ?>"></a></li>
+            </ul></p>
+          </div>
+        </div>
 
-
-
-
+         <!-- Secretary -->
+        <div class="item">
+          <img src="img/officers/<?php print($officers_secretary_image); ?>" alt="<?php print($officers_secretary_name); ?>" />
+          <div class="carousel-caption">
+            <h3>Secretary</h3>
+              <h1><a target="_blank" href="<?php print($officers_secretary_link); ?>"><?php print($officers_secretary_name); ?></a></h1>
+            <p>
+              <strong>Class: </strong> <?php print($officers_secretary_class); ?><br>
+              <strong>Hometown: </strong> <?php print($officers_secretary_hometown); ?>
+            </p>
+            <ul>
+                <li>
+                  <strong><?php print($officers_secretary_question); ?></strong><br>
+                  <?php print($officers_secretary_answer); ?>
+                </li>
+            </ul>
+            <p><ul class="contact-icons" style="text-align: center;">
+                <li><a class="<?php print($officers_secretary_contact); ?>" target="_blank" href="<?php print($officers_secretary_contact_link); ?>"></a></li>
+            </ul></p>
+          </div>
+        </div>
 
     </div>
     <!--  next and previous controls here
